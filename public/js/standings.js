8bd6b6600dace0210f42ee0f86aaa664eba5c586
james I <james.illius@yahoo.com>
2018-08-16 21:28:38 -0400

    added standings html and js file. WIP table create dynamically

---
+++ public/js/standings.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var tableRow = $("<tr>"+ + "</tr>");
+    var tableData = $("<td>"+ + "</td>");
+    var member = "";
+    var email = "";
+    var group = "";
+    var calories = "";
+    var goalCompleted = "";
+
+    
+});
